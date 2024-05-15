da3d83fdd0774c35480b72d5551c0eb90e674979
Houda Rais <houdarais@Houdas-Air.fios-router.home>
2024-05-15 15:00:45 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, createContext, useContext } from "react";
 
-function App() {
+import "./App.css";
+
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+
+// Create a context
+export const MovieContext = createContext();
+
+export default function App() {
+  // Constant with your API Key
+
+  const apiKey = "bf11e9de";
+
+  // State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  // Function to get movies
+  const getMovie = async (searchTerm) => {
+    try {
+      // Make fetch request and store the response
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      // Parse JSON response into a JavaScript object
+      const data = await response.json();
+      // Set the Movie state to the received data
+      setMovie(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const randomMovie = async () => {
+    const response = await fetch(
+      `http://www.omdbapi.com/?s=series&apikey=${apiKey}`
+    );
+    const data = await response.json();
+    console.log(data);
+
+    const randomIndex = Math.floor(Math.random() * data.Search.length);
+
+    // Retrieve a random movie based on the random index
+    const randomMovie = data.Search[randomIndex];
+
+    // Set the movie state with the selected random movie
+    setMovie(randomMovie);
+  };
+
+  // This will run on the first render but not on subsquent renders
+  useEffect(() => {
+    randomMovie();
+  }, []);
+
+  // We pass the getMovie function as a prop called moviesearch
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MovieContext.Provider value={{ movie, getMovie }}>
+      <div className="App">
+        <Form />
+        <MovieDisplay />
+      </div>
+    </MovieContext.Provider>
   );
 }
-
-export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useContext } from "react";
+import { MovieContext } from "../App";
+
+export default function Form() {
+  const { getMovie } = useContext(MovieContext);
+
+  // State to hold the data of our form
+  const [formData, setFormData] = useState({
+    searchterm: "",
+  });
+
+  // handleChange - updates formData when we type into form
+  const handleChange = (event) => {
+    // Use the event object to detect key, and value to update
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    // Prevent page from refreshing on form submission
+    event.preventDefault();
+    // Pass the search term to moviesearch prop, which is App's getMovie function
+    getMovie(formData.searchterm);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="searchterm"
+          onChange={handleChange}
+          value={formData.searchterm}
+        />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
+}
